66b12b02930baadf282797eac28757d48b42bf6d
syler <slavik_br@live.ru>
2016-03-21 20:11:34 +0200

    removed unnecessary overloading in BitHelper

--- DES/BitHelper.cs
+++ DES/BitHelper.cs
@@ -7,17 +7,7 @@ namespace DES
 {
     static class BitHelper
     {
-        public static void PrintBitArray(BitArray array)
-        {
-            foreach (bool item in array)
-            {
-                Console.Write(item ? "1" : "0");
-            }
-
-            Console.WriteLine();
-        }
-
-        public static void PrintBitArray(List<bool> bits)
+        public static void PrintBitArray(IList<bool> bits)
         {
             foreach (var item in bits)
             {
@@ -27,18 +17,7 @@ namespace DES
             Console.WriteLine();
         }
 
-        public static BitArray ShiftLeft(BitArray aSource, int shiftOn)
-        {
-            bool[] new_arr = new bool[aSource.Count];
-            for (int i = 0; i < aSource.Count - shiftOn; i++)
-            {
-                new_arr[i] = aSource[i + shiftOn];
-            }
-
-            return new BitArray(new_arr);
-        }
-
-        public static List<bool> ShiftLeft(List<bool> aSource, int shiftOn)
+        public static List<bool> ShiftLeft(IList<bool> aSource, int shiftOn)
         {
             bool[] new_arr = new bool[aSource.Count];
             for (int i = 0; i < aSource.Count - shiftOn; i++)
